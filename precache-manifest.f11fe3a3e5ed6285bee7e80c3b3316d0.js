d9546dc1db51542027a3a5af66e3079d09b2163b
KudohAtsuo <atsuokudoh@gmail.com>
2018-11-30 11:13:48 +0900

    Updates

---
+++ precache-manifest.f11fe3a3e5ed6285bee7e80c3b3316d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "25e7de9ddcdbcdcb5969",
+    "url": "/lottery-react-dapp/static/css/main.a9b35f83.chunk.css"
+  },
+  {
+    "revision": "25e7de9ddcdbcdcb5969",
+    "url": "/lottery-react-dapp/static/js/main.25e7de9d.chunk.js"
+  },
+  {
+    "revision": "23b93e16d172b34955c1",
+    "url": "/lottery-react-dapp/static/js/1.23b93e16.chunk.js"
+  },
+  {
+    "revision": "fdd48ecaaa94faea069d",
+    "url": "/lottery-react-dapp/static/js/runtime~main.fdd48eca.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lottery-react-dapp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "579c3ba4a8e8e871bc28128edcbca29d",
+    "url": "/lottery-react-dapp/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-pages/precache-manifest.f2c04b3d97513115f6ae419e1400c306.js"
+  "/lottery-react-dapp/precache-manifest.f11fe3a3e5ed6285bee7e80c3b3316d0.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-pages/index.html", {
+workbox.routing.registerNavigationRoute("/lottery-react-dapp/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
